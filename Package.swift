62b94f8d763364bbb2a5f88ca364677228f283a8
N3v1 <n3v1010@gmail.com>
2023-09-16 19:49:07 +0200

    Configure .gitignore

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CMTerminal",
+    platforms: [
+        .macOS(.v13) // TODO: Update v13 (Ventura) to v14 (Sonoma)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "CMTerminal",
+            targets: ["CMTerminal"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "CMTerminal"),
+        .testTarget(
+            name: "CMTerminalTests",
+            dependencies: ["CMTerminal"]),
+    ]
+)
